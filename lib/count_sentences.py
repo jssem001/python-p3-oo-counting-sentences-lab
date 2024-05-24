0d137704afbcbb4146b0f56c19a1689466562115
Joshua Ssemwanga <jssem001@plattsburgh.edu>
2024-05-24 03:16:27 +0300

    strings

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+  def __init__(self, value=""):
+    self.value = value
+  
+  def test_value_string(self):
+      if not isinstance(self.value, str):
+        print("The value must be a string.")
+    
+  def is_sentence(self):
+    sentence = self.value.endswith(".")
+    return sentence
+  
+  def is_question(self):
+    question = self.value.endswith("?")
+    return question
+  
+  def is_exclamation(self):
+    exclamation = self.value.endswith("!")
+    return exclamation
+  
+  def count_sentences(self):
+      return len(re.findall(r'[.!?]+', self.value))
+
+
+  
+
+# sample = MyString("sample sentence! Let's see if this works. Will it work?")
+# print(sample.count_sentences())
+
